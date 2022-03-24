6fec7bd9aabd3530a8e6d4eac1073d68a3a61d77
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2022-03-25 00:12:43 +0100

    [LAB02] end

--- LAB02/uts/Boyera_tests.cpp
+++ LAB02/uts/Boyera_tests.cpp
@@ -1,16 +1,29 @@
 #include <gtest/gtest.h>
 #include "Boyera.h"
+#include <string>
 
 using namespace TaskTwo;
 
-class BoyeraTests : public ::testing::Test
+static Boyer sut;
+
+class BoyeraTestsParameterizedTestFixture : public ::testing::TestWithParam<std::tuple<std::string, std::string, int>>
 {
-protected:
-    BoyeraTests() = default;
-    ~BoyeraTests() = override = default;
 };
 
-TEST_F(BoyeraTests,foo)
+TEST_P(BoyeraTestsParameterizedTestFixture, OddYearsAreNotLeapYears)
 {
-
+    auto inputValue = std::get<0>(GetParam());
+    auto inputBase = std::get<1>(GetParam());
+    auto output = std::get<2>(GetParam());
+    EXPECT_EQ(output, sut.boyermoore(inputValue, inputBase));
 }
+
+INSTANTIATE_TEST_SUITE_P(
+        Dec2AnyTestsParameterizedTests,
+        BoyeraTestsParameterizedTestFixture,
+        ::testing::Values(
+                std::make_tuple("ABAAABCD", "ABC", 4),
+                std::make_tuple("AACBEG", "BEG", 4),
+                std::make_tuple("Jarekmakota", "ako", 6)));
+
+
